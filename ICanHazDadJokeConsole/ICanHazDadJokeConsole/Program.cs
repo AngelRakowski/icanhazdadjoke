94af37ee2bf1d756c6258d521c3aaebb8c4bc787
Angel Rakowski <angel@creative-tendencies.com>
2019-04-28 20:52:31 -0400

    All non-optional requirements met

--- ICanHazDadJokeConsole/ICanHazDadJokeConsole/Program.cs
+++ ICanHazDadJokeConsole/ICanHazDadJokeConsole/Program.cs
@@ -4,8 +4,11 @@ using System.Threading.Tasks;
 using System.Web;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
-/*
+
+/* REQUIREMENTS
+ * 
  * Create an application using C#, .NET, and optionally ASP.NET that uses the “I can haz dad joke” api (https://icanhazdadjoke.com/api) to display jokes. You are welcome to use more technologies like Angular if you wish but it's not required.
 
 There should be two modes the user can choose between:
@@ -19,17 +22,37 @@ and the matching jokes grouped by length: short (<10 words), medium (<20 words),
 
 namespace ICanHazDadJokeConsole
 {
+    #region Classes
+    // This class is the base class for DadJoke which is how the JSON response is returned from icanhazdadjoke.com/API.
     public class DadJoke
     {
         public string ID;
         public string Joke;
     }
 
+    // This class represents the information that we care about returned from the JSON response from icanhazdadjoke.com/API.
+    // It contains a list of DadJokes returned that contain the Search term the user enters.
+
     public class DadJokes
     {
         public IList<DadJoke> Results;
         public string SearchTerm;
     }
+
+    public class DadJokesSettings
+    {
+        public Dictionary<string, string> _clientSettings;
+        public readonly int _delayBetweenJokes = 10000;
+        public Dictionary<string, string> _querySettings;
+        public readonly string _jokesPerPageLimit = @"30";
+        public DadJokesSettings()
+        {
+            _clientSettings = new Dictionary<string, string>();
+            _clientSettings.Add("Accept", "application/json");
+            _clientSettings.Add("User-Agent", @"My Library (https://github.com/AngelRakowski/icanhazdadjoke)");
+        }
+    }
+    #endregion  
 
     class Program
     {
@@ -44,6 +67,8 @@ namespace ICanHazDadJokeConsole
             var input = Console.ReadLine();
 
             int convertedInput = Convert.ToInt32(input);
+
+            // If the user enters in an input other than 1 or 2, it will keep asking them to re-enter it
             while (convertedInput != 1 && convertedInput != 2)
             {
                 Console.WriteLine("\nInvalid input.\n");
@@ -52,11 +77,14 @@ namespace ICanHazDadJokeConsole
                 convertedInput = Convert.ToInt32(input);
             }
 
+            // Spawn a thread to 
             Task.Run(async () =>
             {
                  await GetDadJokes(convertedInput);
             }).GetAwaiter().GetResult();
 
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
 
@@ -76,36 +104,39 @@ namespace ICanHazDadJokeConsole
                 {
                     string responseBody = "";
 
-                    client.DefaultRequestHeaders.Add("Accept", "application/json");
-
-                    // client.DefaultRequestHeaders.Add("User-Agent", "github.com/AngelRakowski/icanhazdadjoke");
+                    // set up the DadJokesSettings
+                    DadJokesSettings settings = new DadJokesSettings();
+                    foreach (var dadJokeKeyPair in settings._clientSettings)
+                    {
+                        client.DefaultRequestHeaders.Add(dadJokeKeyPair.Key, dadJokeKeyPair.Value);
+                    }
 
                     if (input == 1)
                     {
                         while (true)
                         {
+                            // hit the API, map it to our DadJoke class, display it to the screen, delay 10 seconds, and repeat forever
                             responseBody = await client.GetStringAsync(_baseURL);
                             DadJoke dadJoke = JsonConvert.DeserializeObject<DadJoke>(responseBody);
                             Console.WriteLine(dadJoke.Joke);
-                            await Task.Delay(10000);
+                            await Task.Delay(settings._delayBetweenJokes);
                         }
-
-
-
                     }
                     else
                     {
-                        Console.WriteLine("\nEnter in a search term:");
+                        Console.WriteLine("\nEnter in a search term.");
+                        _searchTerm    = Console.ReadLine();
 
-                        _searchTerm = Console.ReadLine();
-                        var builder = new UriBuilder(_baseURL + "/search");
+                        // Build the parameterized query then spawns a thread to return the responseBody as a string in an asyhonchronous operation. 
+                        var builder    = new UriBuilder(_baseURL + "/search");
                         var query      = HttpUtility.ParseQueryString(builder.Query);
-                        query["term"]  = _searchTerm;
-                        query["limit"] = "30";
+                        query.Add("term", _searchTerm);
+                        query.Add("limit", settings._jokesPerPageLimit);
                         builder.Query  = query.ToString();
                         string url     = builder.ToString();
                         responseBody   = await client.GetStringAsync(url);
 
+                        // Map the responseBody to our DadJokes object
                         DadJokes dadJokes = JsonConvert.DeserializeObject<DadJokes>(responseBody);
                         IList<DadJoke> shortJokes  = new List<DadJoke>();
                         IList<DadJoke> mediumJokes = new List<DadJoke>();
@@ -129,14 +160,17 @@ namespace ICanHazDadJokeConsole
 
                         if (shortJokes.Count != 0)
                         {
+                            Console.WriteLine("Short jokes");
                             FormatAndDisplayDadJokes(shortJokes);
                         }
                         if (mediumJokes.Count != 0)
                         {
+                            Console.WriteLine("Medium jokes");
                             FormatAndDisplayDadJokes(mediumJokes);
                         }
                         if (longJokes.Count != 0)
                         {
+                            Console.WriteLine("Long jokes");
                             FormatAndDisplayDadJokes(longJokes);
                         }
                     }
@@ -152,12 +186,16 @@ namespace ICanHazDadJokeConsole
 
         static void FormatAndDisplayDadJokes(IList<DadJoke> jokes)
         {
+            string searchTerm = @"\b" + _searchTerm + @"\b";
             foreach (DadJoke dadJoke in jokes)
             {
-                int start = dadJoke.Joke.IndexOf(_searchTerm);
-                string formattedJoke = dadJoke.Joke.Insert(start + _searchTerm.Length, "<b>");
-                
-                Console.WriteLine(formattedJoke = formattedJoke.Insert(start, "<b>"));
+                if (Regex.IsMatch(dadJoke.Joke, searchTerm))
+                {
+                    string formattedJoke = Regex.Replace(dadJoke.Joke, searchTerm, _searchTerm.ToUpper());
+
+                    Console.WriteLine(formattedJoke);
+                }
+           
             }
         }
     }
